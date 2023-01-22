2acdbfd686e0d78da38f4e7423562ab14fbbf2a4
imun <imun22@gmail.com>
2023-01-22 14:42:58 +0330

    Add Admin/Home controller

--- src/Behlog.Web.Admin/Areas/Admin/Controllers/HomeController.cs
+++ src/Behlog.Web.Admin/Areas/Admin/Controllers/HomeController.cs
@@ -1,6 +1,19 @@
 namespace Behlog.Web.Admin.Controllers;
 
-public class HomeController
+[Area(WebsiteAreaNames.Admin)]
+[Route("[area]/account")]
+[Authorize]
+public class HomeController : Controller
 {
     
+    
+    public HomeController()
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
 }
