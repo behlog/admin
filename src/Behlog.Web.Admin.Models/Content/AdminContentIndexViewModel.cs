fd60e558fb8cfe553b3e321d02b7fd88301deed9
imuan <imun22@gmail.com>
2023-05-22 13:08:05 +0330

    add AdminContentViewModel

--- src/Behlog.Web.Admin.Models/Content/AdminContentIndexViewModel.cs
+++ src/Behlog.Web.Admin.Models/Content/AdminContentIndexViewModel.cs
@@ -2,7 +2,31 @@ namespace Behlog.Web.Admin.Models;
 
 using Behlog.Core.Models;
 
-public class AdminContentIndexViewModel
+public class AdminContentIndexItemViewModel
 {
-    
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Slug { get; set; }
+    public string? Summary { get; set; }
+    public string? AltTitle { get; set; }
+    public int OrderNum { get; set; }
+    public Guid LangId { get; set; }
+    public Guid ContentId { get; set; }
+    public string? ContentTypeName { get; set; }
+    public string? ContentTypeTitle { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? LastUpdated { get; set; }
+    public string? IconName { get; set; }
+    public string AuthorUserId { get; set; }
+    public string? AuthorUserName { get; set; }
+    public string? AuthorUserDisplayName { get; set; }
+    public int LikesCount { get; set; }
+}
+
+public class AdminContentIndexViewModel : PagedViewModel<AdminContentIndexItemViewModel>
+{
+    public AdminContentIndexViewModel()
+    {
+        Data = new List<AdminContentIndexItemViewModel>();
+    }
 }
